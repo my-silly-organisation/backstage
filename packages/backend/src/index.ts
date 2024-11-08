cfd747e11b39c92d0e149fe95c568c23c21646ad
Engin Diri <engin.diri@ediri.de>
2024-11-08 17:22:27 +0100

    feat: initial commit

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -13,6 +13,7 @@ const backend = createBackend();
 backend.add(import('@backstage/plugin-app-backend/alpha'));
 backend.add(import('@backstage/plugin-proxy-backend/alpha'));
 backend.add(import('@backstage/plugin-scaffolder-backend/alpha'));
+backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
 backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
 
 // auth plugin
